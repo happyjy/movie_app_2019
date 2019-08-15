5f914480f54765d5937d6afc0a5cbd9fd6530632
happyjy <okwoyjy@gmail.com>
2019-08-15 16:30:42 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Potato from './potato';
+// import Potato from './potato';
+
+function Food({favourite, ingredient}) {
+  console.log("### Food component arguments: ", arguments);
+  return <h1> I like {favourite}, {ingredient} </h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>  
-      <Potato></Potato>
+      <Food favourite="kimchi" 
+        ingredient={['cabbage', 'red Pepper']}/>
     </div>
   );
 }
