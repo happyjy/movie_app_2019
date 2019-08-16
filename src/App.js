d29e7672c36a585724126a3b79674f3c91d3fa61
happyjy <okwoyjy@gmail.com>
2019-08-16 23:48:24 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import axios from 'axios';
+
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
 
+  //async, await(ES6)는 javaScript에게 getMovies function을 했을때 시간이 걸려 기다려 달라는 의미로 사용한 것 
+  getMovie = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({ isLoading: false });
-    }, 3000);
+    this.getMovies();
+    // setTimeout(()=>{
+    //   this.setState({ isLoading: false });
+    // }, 3000);
   }
 
   render(){
