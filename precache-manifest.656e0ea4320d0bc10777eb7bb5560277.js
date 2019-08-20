6267f117f541bf02abd7476fd4b07b130d3e19f4
happyjy <okwoyjy@gmail.com>
2019-08-20 23:48:29 +0900

    Updates

---
+++ precache-manifest.656e0ea4320d0bc10777eb7bb5560277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833132f02a578d6e3e1bd85d016771f5",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "406eb0858670e5436455",
+    "url": "/movie_app_2019/static/css/main.1af1ea54.chunk.css"
+  },
+  {
+    "revision": "0412129e6fe003322cd1",
+    "url": "/movie_app_2019/static/js/2.af5468dd.chunk.js"
+  },
+  {
+    "revision": "406eb0858670e5436455",
+    "url": "/movie_app_2019/static/js/main.ed729431.chunk.js"
+  },
+  {
+    "revision": "ba3076a0c8d176265049",
+    "url": "/movie_app_2019/static/js/runtime~main.1d21743e.js"
+  }
+]);
